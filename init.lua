af1497fc469b39cc2c22d21676565884308814ca
Qianyu Zhou <qianyu@Qianyus-MacBook-Air.local>
2023-06-24 09:33:20 +0900

    init

---
+++ init.lua
@@ -0,0 +1,37 @@
+local set = vim.o
+set.relativenumber = true
+set.number = true
+set.clipboard = "unnamed"
+
+
+-- hightlight the copy
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	pattern = { "*" },
+	callback = function()
+		vim.highlight.on_yank({
+			timeout = 300,
+		})
+	end,
+})
+
+-- keybindings
+vim.g.mapleader = " "
+local opt = { noremap = true, silent = true }
+vim.keymap.set("n", "<leader>l", "<C-w>l", opt)
+vim.keymap.set("n", "<leader>k", "<C-w>k", opt)
+vim.keymap.set("n", "<leader>h", "<C-w>h", opt)
+vim.keymap.set("n", "<leader>j", "<C-w>j", opt)
+vim.keymap.set("n", "<leader>o", "<C-w>o", opt)
+vim.keymap.set("n", "<leader>c", "<C-w>c", opt)
+vim.keymap.set("n", "<leader>p", "<C-w>p", opt)
+
+
+vim.api.nvim_set_keymap('n', 'sl', ':set splitright<CR>:vsplit<CR>', {})
+vim.api.nvim_set_keymap('n', 'sh', ':set nosplitright<CR>:vsplit<CR>', {})
+vim.api.nvim_set_keymap('n', 'sk', ':set nosplitbelow<CR>:split<CR>', {})
+vim.api.nvim_set_keymap('n', 'sj', ':set splitbelow<CR>:split<CR>', {})
+
+
+-- https://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
+vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
+vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true })
