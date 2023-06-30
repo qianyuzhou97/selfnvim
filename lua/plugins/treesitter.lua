59e8c50d610d02ebeb460c0ba942fcbc2863d7b5
Qianyu Zhou <zhou.qianyu@moneyforward.co.jp>
2023-06-30 11:02:25 +0900

    add more treesitter defaults

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		config = function()
 			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "gomod", "gosum", "gowork" },
 				incremental_selection = {
 					enable = true,
 					keymaps = {
